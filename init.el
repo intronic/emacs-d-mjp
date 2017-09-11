6a4c4099e527b304aa12832d650814a88f161601
Mike Pheasant <mike.pheasant@ephox.com>
2017-09-11 23:05:56 +1000

    workingversion

--- init.el
+++ init.el
@@ -1,6 +1,16 @@
 ;; http://melpa.org/#/getting-started
-
 (require 'package) ;; You might already have this line
+
+;;(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+;;                    (not (gnutls-available-p))))
+;;       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+;;  (add-to-list 'package-archives (cons "melpa" url) t))
+;;(when (< emacs-major-version 24)
+;;  ;; For important compatibility libraries like cl-lib
+;;  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+;;(package-initialize) ;; You might already have this line
+
+
 (setq load-prefer-newer t
       package-enable-at-startup nil
       package-archives
@@ -54,8 +64,8 @@
 ;;             (enable-theme 'solarized)))
 
 ;; https://github.com/purcell/exec-path-from-shell
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
+;;(when (memq window-system '(mac ns x))
+;;  (exec-path-from-shell-initialize))
 
 ;; http://www.flycheck.org/
 (use-package flycheck
@@ -75,3 +85,18 @@
 ;; (add-hook 'dante-mode-hook
 ;;    '(lambda () (flycheck-add-next-checker 'haskell-dante
 ;;                 '(warning . haskell-hlint))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(frame-background-mode (quote dark))
+ '(package-selected-packages
+   (quote
+    (dante flycheck use-package f color-theme-solarized))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
